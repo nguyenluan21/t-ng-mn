e0c66cf1d92bc8a10350ecccb203feceb2748855
Nguyễn Luân <nguyenluan21qaz@gmail.com>
2021-09-29 12:06:34 +0700

    add README

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Hello mọi người!',
+    introDesc: `cái này mang tính chất vui vẻ`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Phải chăng bạn bị điên',
+    desc: 'Nói thật đi',
+    btnYes: 'Đúng vậy ',
+    btnNo: 'Không phải',
+    question:'Lý do bạn bị điên',
+    btnReply: 'Lý do',
+    reply: 'Tôi thừa nhận rồi nên không cí lý do j hết',
+    mess: 'Tôi biết mà',
+    messDesc: 'hahaha',
+    btnAccept: 'haiza',
+    messLink: 'https://www.facebook.com/luan.kenny.2001/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
